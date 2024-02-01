ed369ca5b585704a9dcf6b97cdc0d6e9f1b65db0
nino <nino@ninos-MacBook-Air.local>
2024-02-01 22:05:47 +0400

    add components and pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="bg-red-200 fixed w-full px-[30px] lg:px-[100px] z-[100px] lg:h-[140px] flex items-center">
-      header nino
+      header
     </header>
   );
 };
